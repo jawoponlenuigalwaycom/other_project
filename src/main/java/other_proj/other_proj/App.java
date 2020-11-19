822077ef1a58d68200538539bbe9270cf07f437d
jawoponlenuigalwaycom <56373264+jawoponlenuigalwaycom@users.noreply.github.com>
2020-11-19 23:45:56 +0000

    initial commit

---
+++ src/main/java/other_proj/other_proj/App.java
@@ -0,0 +1,13 @@
+package other_proj.other_proj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/other_proj/other_proj/Main.java
@@ -0,0 +1,5 @@
+package other_proj.other_proj;
+
+public class Main {
+
+}
